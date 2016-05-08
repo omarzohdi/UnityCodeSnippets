30dc1bce86639314372f23cd445b5417f6482f21
Omar Zohdi <omarzohdi@gmail.com>
2016-05-08 02:13:31 -0500

    Testing GoogleCardBoard SDK
    Playing around with the GoogleCardboard SDK.

---
+++ GoogleCardBoardTest/Assets/ChangeColorOnGaze.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColorOnGaze : MonoBehaviour {
+
+    private Color startColor;
+    private Color endColor;
+
+    // Use this for initialization
+    void Start ()
+    {
+        startColor = gameObject.GetComponent<Renderer>().material.color;
+        endColor = Color.red;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void ChangeColorOnEnter ()
+    {
+        gameObject.GetComponent<Renderer>().material.color = endColor;
+    }
+
+    public void ChangeColorOnExit()
+    {
+        gameObject.GetComponent<Renderer>().material.color = startColor;
+    }
+}
+
