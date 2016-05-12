fe01dfa6abc2139f84fc1372321242d91ab1f8fa
Omar Zohdi <omarzohdi@gmail.com>
2016-05-12 04:02:35 -0500

    UI Project
    Project with code snippets for UIs

---
+++ UI/Assets/Scripts/Interactable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour {
+
+    [System.Serializable] //make editable in inspector
+    public class Action
+    {
+        public Color color;
+        public Sprite sprite;
+        public string title;
+    }
+
+    public Action[] options;
+
+	void OnMouseDown()
+    {
+        //tell Canvas to Spawn a Menu;
+    }
+}
---
+++ UI/Assets/Scripts/MouseRayCast.cs
@@ -0,0 +1,53 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class MouseRayCast : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left Click");
+            RayCast3D();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Right Click");
+            RayCast2D();
+        }
+    }
+
+    //Ray Cast to a Sprite!
+    public static bool RayCast2D()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+      
+        if (hit)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    //Ray Cast to a 3d Object
+    public static bool RayCast3D()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
