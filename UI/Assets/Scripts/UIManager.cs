9de8107bf3ca05a121103d597dc229eb94da3439
Omar Zohdi <omarzohdi@gmail.com>
2016-05-13 20:19:06 -0500

    UI manager
    Created a UI Manager that initializes all required Prefabs and Game
    Objects for Radial Menus

---
+++ UI/Assets/Scripts/UIManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+    private static GameObject UIContainer;
+    private static UIManager _instance;
+    public GameObject eventSystem;
+
+    #region Initialization
+    public static UIManager Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                _instance = GameObject.FindObjectOfType<UIManager>();
+                if (!_instance)
+                {
+                    UIContainer = new GameObject();
+                    UIContainer.name = "UIContainer";
+                    _instance = UIContainer.AddComponent(typeof(UIManager)) as UIManager;
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    private void SetupUIContainer()
+    {
+        UIContainer = GameObject.Find("UIContainer");
+        if (UIContainer == null)
+        {
+            UIContainer = new GameObject();
+            UIContainer.name = "UIContainer";
+        }
+    }
+    private void SetupEventSystem()
+    {
+        EventSystem _eventSystem = FindObjectOfType<EventSystem>();
+        if (_eventSystem == null)
+        {
+            eventSystem = new GameObject();
+            eventSystem.name = "EventSystem";
+            eventSystem.AddComponent<EventSystem>();
+            eventSystem.AddComponent<StandaloneInputModule>();
+        }
+        else
+            eventSystem = _eventSystem.gameObject;
+
+        eventSystem.transform.SetParent(UIContainer.transform);
+    }
+
+    public void Initialize()
+    {
+        this.SetupUIContainer();
+        this.SetupEventSystem();
+        this.SetupRadialMenuCanvas();
+    }
+    #endregion
+
+    #region RadialMenus
+
+    public GameObject radMenuCanvas;
+
+    private void SetupRadialMenuCanvas ()
+    {
+        RadialMenuSpawner MenuSpawn = FindObjectOfType<RadialMenuSpawner>();
+        if (MenuSpawn == null)
+            radMenuCanvas = Instantiate(radMenuCanvas) as GameObject;
+        else
+            radMenuCanvas = MenuSpawn.gameObject;
+
+        radMenuCanvas.transform.SetParent(UIContainer.transform);     
+    }
+
+    #endregion
+
+    void Awake()
+    {
+        this.Initialize(); 
+    }
+}
