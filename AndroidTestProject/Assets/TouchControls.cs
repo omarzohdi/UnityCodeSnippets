c8c4f8f87c8ed27e2cf2937b8d47cf6364682de2
Omar Zohdi <omarzohdi@gmail.com>
2016-05-16 03:44:30 -0500

    Two More Projects
    Added an Android test project and a Unity Editor Scripting project

---
+++ AndroidTestProject/Assets/TouchControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchControls : MonoBehaviour {
+
+    private Touch touch;
+
+	// Use this for initialization
+	void Start () { }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        touch = Input.GetTouch(0);
+        
+        Vector3 temp = Camera.main.GetComponent<Camera>().ScreenToWorldPoint(touch.position);
+
+        this.GetComponent<BoxCollider>().bounds.Contains(temp);
+       
+    }
+}
